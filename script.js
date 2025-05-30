167ddaf15b8031ff373c2d23af9e8689385cd60c
transuranium <ayavonaamimi@gmail.com.com>
2025-05-30 00:11:40 -0500

    fixed things around form

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ document
       text += `*${key}:* ${value}%0A`;
     }
 
-    const TOKEN = "TU_BOT_TOKEN"; // ← заменим позже
-    const CHAT_ID = "TU_CHAT_ID"; // ← заменим позже
+    const TOKEN = "7778169971:AAG1STrB4IvAzFWfAwdnaklqBCr_nvfV1X0";
+    const CHAT_ID = "221555421";
     const URL = `https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${text}&parse_mode=Markdown`;
 
     fetch(URL)
